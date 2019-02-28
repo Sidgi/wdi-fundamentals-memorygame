bb9f7b0c4bab1ddf223386652776948f7c717d05
SvS <samanduyev@gmail.com>
2019-02-28 01:37:40 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
 console.log("Up and running!");
-let cardOne = "king";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "queen";
 
-console.log("User flipped "+ cardOne);
+// console.log("User flipped "+ cardOne);
+cards = ['queen','queen','king','king'];
+cardsInPlay = [];
+let cardOne = cards[0];
+let cardTwo = cards[1];
+// let cardThree = cards[2];
+// let cardFour = cards[3];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+// cardsInPlay.push(cardThree);
+// cardsInPlay.push(cardFour);
+console.log(cardsInPlay);
+if(cardsInPlay.length===2){
+  if(cardsInPlay[0] ===cardsInPlay[1]){
+    alert("You found match!");
+  }
+  else{
+    alert("Sorry,try again");
+  }
+}
